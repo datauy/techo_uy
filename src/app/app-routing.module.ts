288106ead4840b0982d128d025086f9553a77221
Danilo Espino <daniloespino@gmail.com>
2019-02-08 19:48:07 -0300

    Subpages done.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { MethodologyComponent } from './methodology/methodology.component';
+import { ContactComponent } from './contact/contact.component';
+import { PressRoomComponent } from './press-room/press-room.component';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
+  { path: 'methodology', component: MethodologyComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'press-room', component: PressRoomComponent },
   { path: '', redirectTo: '/map', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { MethodologyComponent } from './methodology/methodology.component';
+import { PressRoomComponent } from './press-room/press-room.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    MethodologyComponent,
+    PressRoomComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
