547b312fe54898866c64e80c5aa267eb1645ea89
Fernando Uval <fernandouval@gmail.com>
2019-02-27 10:45:33 -0300

    Función para cerrar menú en click, fuente Open Sans

--- src/js/controllers/abs.ctrl.js
+++ src/js/controllers/abs.ctrl.js
@@ -1,5 +1,7 @@
 pmb_im.controllers.controller('AbsController',
 ['$scope',
 function($scope) {
-
+  $scope.menuClick = function() {
+    document.getElementById("menu-btn").checked = false;
+  };
 }]);
